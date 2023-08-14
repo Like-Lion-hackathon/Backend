c799a931759f86677673a2498c057ed77aee731b
서병덕 <qudejr0000@naver.com>
2023-08-15 00:49:19 +0900

    통합코드

---
+++ src/main/java/com/example/hackathon/Entity/mapping.java
@@ -0,0 +1,25 @@
+package com.example.hackathon.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "mapping")
+//post - subcategory
+public class mapping {
+
+    @Id
+    private Long mapping_id;
+
+    @ManyToOne
+    private Subcategory subcategory_id;
+
+    @ManyToOne
+    private Post post_id;
+}
