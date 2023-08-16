d151d898d7b285c082465b1ec89ad4793dcfbae9
서병덕 <qudejr0000@naver.com>
2023-08-17 00:41:56 +0900

    로그인 완

--- src/main/java/com/example/hackathon/Controller/loginController.java
+++ src/main/java/com/example/hackathon/Controller/loginController.java
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class loginController {
 
-    @GetMapping("/login")
-    public String showNumberInputPage() {
-        return "index"; // login.html 연결
-    }
+//    @GetMapping("/login")
+//    public String showNumberInputPage() {
+//        return "index"; // login.html 연결
+//    }
 
-    @PostMapping("/login2")
+    @PostMapping("/login")
     public ResponseEntity processNumber(@RequestParam("id") String id, Model model) {
         if ("1234512345".equals(id) ){ // 예시 조건: 번호가 "1234512345"일 때만 다음 페이지로 이동
-            return ResponseEntity.ok("로그인 성공");
+            return ResponseEntity.ok(200);
         } else {
             return  ResponseEntity.badRequest().body("로그인 실패");
         }
