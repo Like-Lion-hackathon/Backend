09d1f7fc2001eb6d87a3f3496c211d01025f0ba6
kimhana11 <117277958+kimhana11@users.noreply.github.com>
2023-08-18 02:05:24 +0900

    Update PostController.java

--- src/main/java/com/example/hkp/Controller/PostController.java
+++ src/main/java/com/example/hkp/Controller/PostController.java
@@ -21,12 +21,17 @@ public class PostController {
 
 
     //게시물 정보 전부 조회
-    @GetMapping("/postData")
+    @GetMapping("/ListData")
   //  @ResponseBody  ("key[]")
     public List<Map<String, Object>> getPostDetails(@RequestParam List<Integer> subcategoryIds) {
         return postRepository.getPostDetails(subcategoryIds);
     }
-
+    
+   @GetMapping("/postData")
+    //  @ResponseBody  ("key[]")
+    public List<Map<String, Object>> getSub(@RequestParam("0") Integer s1,@RequestParam("1") Integer s2,@RequestParam("2") Integer s3,@RequestParam("3") Integer s4 ) {
+        return postRepository.getSub(s1, s2, s3, s4);
+    }
     //test
     @RequestMapping(value="insert", method = RequestMethod.POST)
     public @ResponseBody List<Map<String,Object>> insert(@RequestBody List<Map<String,Object>> subcategoryIds) {
